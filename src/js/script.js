5f59043b77cfcdfa463db9c3df149494ca56e534
Amantay <amantay.sv@gmail.com>
2021-10-12 21:05:14 +0600

    added trusted carousel

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,10 @@ $('.brands__carousel').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
-  speed: 2500
+  speed: 2500,
+})
+$('.trusted-carousel').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  variableWidth: true,
 })
