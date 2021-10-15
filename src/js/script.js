cf94d99e0baa87b25257968156c5db2cb02fe9ee
Amantay <amantay.sv@gmail.com>
2021-10-15 19:27:00 +0600

    fixed media

--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,31 @@ $('.brands__carousel').slick({
   autoplay: true,
   autoplaySpeed: 3000,
   speed: 2500,
+  responsive: [
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 3,
+        arrows: false,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 })
 
 $('.trusted-carousel').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        variableWidth: false,
+      },
+    },
+  ],
 })
